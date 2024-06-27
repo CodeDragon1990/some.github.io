af6a73fedbfa1d44a5cccef7b76b2ac53f7ed01d
CodeDragon1990 <168695221+CodeDragon1990@users.noreply.github.com>
2024-06-27 20:47:32 +0800

    some.github.io

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function goToFlagPage2() {
+  console.log('前往开发者立过的flag页面');
+  // 实现页面跳转逻辑
+}
+
+function goToFlagPage() {
+  console.log('前往开发者立过的flag页面');
+  // 实现页面跳转逻辑
+}
+
+function goToSponsorPage() {
+  console.log('前往赞助开发者页面');
+  // 实现页面跳转逻辑
+}
+
+function goToVotePage() {
+  console.log('前往给偶像投票页面');
+  // 实现页面跳转逻辑
+}
